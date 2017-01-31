2ba1cbd8a4b39226c572dd0dad924b4e9bb4e553
hvkhodifad123 <hvkhodifad123@gmail.com>
2017-01-31 12:43:02 +0530

    chat_application

---
+++ chat.js
@@ -0,0 +1,44 @@
+ifUserIsLoggedIn(function () {
+    updateUserData();
+
+    loadUsers(function(users) {
+        var usersList = "";
+
+        for (var uid in users) {
+            var user = users[uid];
+            if (window.currentUser.id != uid) {
+                usersList += renderUser(user);
+            }
+        }
+        getElement("members").innerHTML = usersList;
+    });
+
+    onClickMultiple("member", function(element) {
+        var chat_id = element.id;
+        var chaterElement = getElement("chater");
+        chaterElement.textContent = element.textContent;
+
+        loadMessages(chat_id,function(messages) {
+            var messagesList = "";
+
+            for (var uid in messages) {
+                var message = messages[uid];
+                messagesList += renderMessage(message);
+            }
+            getElement("messages").innerHTML = messagesList;
+            //console.log(messagesList);
+        });
+
+        getElement("chat-id").value = chat_id;
+
+    });
+
+    click("send-button", function() {
+        var text = getElement("message-text").value;
+        var chat_id = getElement("chat-id").value;
+
+        sendMessage(chat_id, text);
+
+    });
+
+});
